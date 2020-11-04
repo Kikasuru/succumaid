bf168477ce33d835a1af57d8e6d7300897342ec7
Kiksuru <tgvaporeon@gmail.com>
2020-11-04 14:25:02 -0600

    Added sprites

--- public/js/spr.js
+++ public/js/spr.js
@@ -1,5 +1,85 @@
 // spr.js - Handles sprite loading and fonts
 let letters = "!\"#$%'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQUSTUVWXYZ[\\]^_abcdefghijklmnopqrstuvwxyz{|}~"
+let letters = "!\"#$%'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQUSTUVWXYZ[\\]^_abcdefghijklmnopqrstuvwxyz{|}~";
+
+class Sprite {
+    constructor(layers, sw, sh){
+        this.layers = layers;
+        this.sw     = sw;
+        this.sh     = sh;
+    }
+
+    render(){
+        let spr = this;
+
+        return new Promise(function(res, reg) {
+            let imgsLoaded = 0
+
+            // Create new images for each layer
+            spr.layers.forEach(function(e) {
+                e.img = new Image();
+                e.img.src = e.src;
+
+                e.img.onload = function() {
+                    imgsLoaded++
+
+                    // When all images are loaded, put them together
+                    if (imgsLoaded === spr.layers.length) assemble();
+                }
+            });
+
+            function assemble(){
+                // Check if the width and height are the same for each layer.
+                if(spr.layers.every(function(e) {
+                    return spr.layers[0].img.width === e.img.width && spr.layers[0].img.height === e.img.height
+                })) {
+                    // Make a new canvas for this sprite
+                    spr.canvas = document.createElement("canvas");
+
+                    // Resize this canvas
+                    spr.canvas.width   = spr.layers[0].img.width;
+                    spr.canvas.height  = spr.layers[0].img.height;
+
+                    // Add this canvas to the sprite cache
+                    document.getElementById("spr").appendChild(spr.canvas);
+
+                    // Make a new context for the canvas
+                    let ctx = spr.canvas.getContext("2d");
+
+                    // Draw each layer onto the canvas
+                    spr.layers.forEach(function(e) {
+                        ctx.drawImage(e.img, 0, 0);
+
+                        // Color this layer
+                        recolorArea(ctx, e.color, 0, 0, e.img.width, e.img.height);
+                    });
+
+                    // Resolve the promise
+                    res(true);
+                } else {
+                    // If not, reject this Promise
+                    rej("Layers are varied sizes");
+                }
+            }
+        });
+    }
+
+    draw(ctx, sprid, x, y) {
+        // Make sure the canvas exists
+        if (this.canvas) {
+            // Draw the sprite
+            ctx.drawImage(this.canvas,
+                // Source
+                (sprid % (this.canvas.width / this.sw)) * this.sw, Math.floor(sprid / (this.canvas.width / this.sw)) * this.sw, this.sw, this.sh,
+                // Destination
+                x, y, this.sw, this.sh
+            );
+        } else {
+            // If not, return false
+            return false;
+        }
+    }
+}
 
 class BitFont {
     constructor(img, widths, space){
