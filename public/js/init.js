2712f9d1c35dc68d0a169ce298dccea271d31613
Kiksuru <tgvaporeon@gmail.com>
2020-11-03 23:54:29 -0600

    Moved a couple things around

--- public/js/init.js
+++ public/js/init.js
@@ -26,36 +26,42 @@ function init(){
         // Load the palette
         let palimg = new Image();
         palimg.src = "assets/pal.png";
+
+        // Create a new font
+        let fontimg = new Image();
+        fontimg.src = "assets/font.png"
+        let font = new BitFont(fontimg, "46987333673737747777777733575787777777757778777777777977747457777777773573977777777797775355", 4);
+
         palimg.onload = function(){
             createCHexObject(palimg, 0);
 
-            // Create a new font
-            let fontimg = new Image();
-            fontimg.src = "assets/font.png"
-            let font = new BitFont(fontimg, "46987333673737747777777733575787777777757778777777777977747457777777773573977777777797775355", 4);
             fontimg.onload = function(){
                 font.render();
 
-                let currColor = color.WHITE;
-
                 // Start the loop
-                setInterval(function(){
-                    // Clear the screen
-                    ctx.fillStyle = coltohex(colorhex[color.BLACK][3]);
-                    ctx.fillRect(0,0,(window.innerWidth/zoom),(window.innerHeight/zoom));
-
-                    // If the mouse was clicked..
-                    if (mouse.press.left || mouse.press.right) {
-                        // ..change the color!
-                        currColor = (currColor % 14) + 1;
-                    }
-
-                    font.draw(ctx, "The quick brown fox jumps over the lazy dog.", mouse.x, mouse.y, currColor);
-
-                    // Reset presses
-                    mouse.resetPresses();
-                },16);
+                setInterval(main,16);
             }
+        }
+
+
+        // Main Loop
+        let currColor = color.WHITE;
+
+        function main() {
+            // Clear the screen
+            ctx.fillStyle = coltohex(colorhex[color.BLACK][3]);
+            ctx.fillRect(0,0,(window.innerWidth/zoom),(window.innerHeight/zoom));
+
+            // If the mouse was clicked..
+            if (mouse.press.left || mouse.press.right) {
+                // ..change the color!
+                currColor = (currColor % 14) + 1;
+            }
+
+            font.draw(ctx, "The quick brown fox jumps over the lazy dog.", mouse.x, mouse.y, currColor);
+
+            // Reset presses
+            mouse.resetPresses();
         }
     } else {
         // Tell the user that their browser is not Canvas compatible.
